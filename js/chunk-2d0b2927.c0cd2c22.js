d36f4b4739eeb145706fa86d3b2f54b70bbeb6c5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-04 14:43:01 +0000

    Deploy kathirr007/Vuetify-material-dashboard-pro-clone to github.com/kathirr007/Vuetify-material-dashboard-pro-clone.git:gh-pages

---
+++ js/chunk-2d0b2927.c0cd2c22.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b2927"],{"254b":function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("h1",{staticClass:"display-4 font-weight-light"},[t._t("default")],2)},l=[],s={name:"PagesHeading"},i=s,u=n("2877"),c=Object(u["a"])(i,a,l,!1,null,null,null);e["default"]=c.exports}}]);
+//# sourceMappingURL=chunk-2d0b2927.c0cd2c22.js.map
---
+++ js/chunk-2d0c4bbd.c048b6a5.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c4bbd"],{"3ba9":function(e,n,t){"use strict";t.r(n);var a,l,o=t("8336"),d={name:"PagesBtn",extends:o["a"],props:{color:{type:String,default:"success"},depressed:{type:Boolean,default:!1},default:{type:Boolean,default:!0},rounded:{type:Boolean,default:!0}}},u=d,s=t("2877"),p=Object(s["a"])(u,a,l,!1,null,null,null);n["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d0c4bbd.c048b6a5.js.map
